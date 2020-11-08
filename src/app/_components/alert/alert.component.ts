6fc7f2167641df007b3f9c8ecf24570f383da927
claudiuPunch <67048726+claudiuPunch@users.noreply.github.com>
2020-11-08 12:01:30 +0100

    completed service

--- src/app/_components/alert/alert.component.ts
+++ src/app/_components/alert/alert.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Alert, AlertType } from '@app/_models';
+import { AlertService } from '@app/_services';
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css']
 })
-export class AlertComponent implements OnInit {
-
+export class AlertComponent implements OnInit, OnDestroy {
+  @Input() id = 'default-alert';
+  @Input() fade = true;
+  alerts: Alert[] = [];
   constructor() { }
 
   ngOnInit(): void {
