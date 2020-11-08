7386a9014f4446eae69be0b432d52df72163752b
claudiuPunch <67048726+claudiuPunch@users.noreply.github.com>
2020-11-08 12:45:15 +0100

    completed alert functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AlertComponent } from './_components/alert/alert.component';
+import { AlertComponent } from './_components/alert.component';
 
 @NgModule({
   declarations: [
